3e885f981129e86bb19d78eaba80a3944533e6d9
ahmedhesham001 <ahmed.106300@cs.mti.edu.eg>
2024-09-15 21:30:31 +0300

    javascript updated

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-//  https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=
-let imgBox = document.getElementById("imgBox"); 
-let qrImg = document.getElementById("qrImg"); 
-let input = document.getElementById("qrTxt");
-let button = document.querySelector("button");
-function generateQr() {
-    qrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+input.value;
-}
-button.addEventListener("click",generateQr);
+let imgBox = document.getElementById("imgBox"), 
+qrImg = document.getElementById("qrImg"), 
+input = document.getElementById("qrTxt"),
+button = document.querySelector("button");
+button.addEventListener("click",()=>
+    qrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+input.value);
